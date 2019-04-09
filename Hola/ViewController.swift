7770c58b1ac4770799c8320891ce3dad34100744
Kike Algarra <kike@areavisual.net>
2019-04-09 20:57:54 +0200

    ViewController.swift
    ViewController.swift.

---
+++ Hola/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hola
+//
+//  Created by bykike on 19/01/2019.
+//  Copyright © 2019 bykike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
