b09fa10b62d82dbbd1baadec594845318adf4fe5
Kike Algarra <kike@areavisual.net>
2019-04-12 19:47:26 +0200

    XCTest
    XCTest.

---
+++ HolaTests/HolaTests.swift
@@ -0,0 +1,34 @@
+//
+//  HolaTests.swift
+//  HolaTests
+//
+//  Created by bykike on 19/01/2019.
+//  Copyright © 2019 bykike. All rights reserved.
+//
+
+import XCTest
+@testable import Hola
+
+class HolaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
